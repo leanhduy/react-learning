e14b735e2764de01aa6cf7bcb77db5bbac177143
leanhduy <laduy.hcmus@gmail.com>
2024-04-20 21:47:26 +0700

    implement the input of decimal point

--- src/components/mini2/CalcApp.js
+++ src/components/mini2/CalcApp.js
@@ -1,12 +1,19 @@
 import { Box, Container, Grid, Typography } from '@mui/material'
-import { BUTTON_BG, EQUAL_BG, WRAPPER_BG } from './colors'
-import CalcScreen from './CalcScreen'
-import CalcBtn from './CalcBtn'
 import { useEffect, useState } from 'react'
-import { MATH_OPERATORS, NUMBERS, calculate } from './calculatorUtils'
-import CalcClearBtn from './CalcClearBtn'
-import EqualBtn from './EqualBtn'
 import toastr from 'toastr'
+import CalcBtn from './CalcBtn'
+import CalcClearBtn from './CalcClearBtn'
+import CalcScreen from './CalcScreen'
+import EqualBtn from './EqualBtn'
+import {
+    DECIMAL_POINT,
+    MATH_OPERATORS,
+    NUMBERS,
+    SIGN_OPERATORS,
+    calculate,
+} from './calculatorUtils'
+import { BG, BUTTON_BG, EQUAL_BG, WRAPPER_BG } from './colors'
+import beepEffect from '../../sounds/beep.mp3'
 
 const BUTTONS = [
     '+-',
@@ -30,13 +37,22 @@ const BUTTONS = [
 
 const CalcApp = () => {
     const [prevInputType, setPrevInputType] = useState(null)
+    const [decimalCount, setDecimalCount] = useState(0) // To prevent user input two decimal point in a number
     const [expression, setExpression] = useState([])
+    const [decimalPart, setDecimalPart] = useState(0)
     const [result, setResult] = useState(0)
 
-    useEffect(() => {
-        console.log(prevInputType)
-        console.log(expression)
-    }, [result, expression, prevInputType])
+    const playSound = () => {
+        new Audio(beepEffect).play()
+    }
+
+    useEffect(() => {}, [
+        decimalCount,
+        decimalPart,
+        expression,
+        prevInputType,
+        result,
+    ])
 
     const addOperator = (label) => {
         // If it is a new Caculation, reset states
@@ -45,11 +61,15 @@ const CalcApp = () => {
             setResult(0)
         }
 
-        // Get the type of the button on the calculator (number, operator, others)
+        // Get the type of the button on the calculator (number, operator, sign, decimal)
         let inputType = NUMBERS.includes(label)
             ? 'number'
             : MATH_OPERATORS.includes(label)
             ? 'operator'
+            : DECIMAL_POINT.includes(label)
+            ? 'decimal'
+            : SIGN_OPERATORS.includdes(label)
+            ? 'sign'
             : 'others'
 
         // Check the input type and the previous input type to perform appropriate update on the `expression` state
@@ -60,9 +80,36 @@ const CalcApp = () => {
                     // Prev input type = operator. Add new input to expression
                     setExpression([...expression, Number(label)])
                 } else if (prevInputType === 'number') {
-                    // Update the last element of input
-                    let newNum =
-                        expression[expression.length - 1] * 10 + Number(label)
+                    if (decimalCount === 0) {
+                        // Update the last element of input
+                        let newNum =
+                            expression[expression.length - 1] * 10 +
+                            Number(label)
+                        setExpression([
+                            ...expression.slice(0, expression.length - 1),
+                            newNum,
+                        ])
+                    } else {
+                        let newDecimal =
+                            decimalPart +
+                            Number(label) * Math.pow(10, -1 * decimalCount)
+                        let newNum =
+                            expression[expression.length - 1] + newDecimal
+                        setDecimalCount((c) => c + 1)
+                        setDecimalPart(decimalPart)
+                        setExpression([
+                            ...expression.slice(0, expression.length - 1),
+                            newNum,
+                        ])
+                    }
+                } else if (prevInputType === 'decimal') {
+                    // debugger
+                    let newDecimal =
+                        decimalPart +
+                        Number(label) * Math.pow(10, -1 * decimalCount)
+                    let newNum = expression[expression.length - 1] + newDecimal
+                    setDecimalCount((c) => c + 1)
+                    setDecimalPart(decimalPart)
                     setExpression([
                         ...expression.slice(0, expression.length - 1),
                         newNum,
@@ -86,10 +133,29 @@ const CalcApp = () => {
                         label,
                     ])
                     setPrevInputType('operator')
+                } else if (['sign', 'decimal'].includes(prevInputType)) {
+                    playSound()
+                    toastr.warning(
+                        'Please input a number first',
+                        'Invalid input'
+                    )
                 } else {
+                    // previous input is a number
                     // Add new operator to the expression
                     setExpression([...expression, label])
                     setPrevInputType('operator')
+                    setDecimalCount(0)
+                    setDecimalPart(0)
+                }
+                break
+            case 'decimal':
+                if (decimalCount > 0) {
+                    playSound()
+                } else if (['sign', 'operator'].includes(prevInputType)) {
+                    toastr.warning('Please input a number', 'Invalid input')
+                } else {
+                    setDecimalCount((d) => d + 1)
+                    setPrevInputType('decimal')
                 }
                 break
             default:
@@ -111,17 +177,24 @@ const CalcApp = () => {
         <Box
             sx={{
                 background:
-                    'linear-gradient(0deg, rgba(55,151,200,1) 30%, rgba(252,176,48,1) 100%)',
+                    'linear-gradient(0deg, rgba(251,176,52,1) 0%, rgba(91,73,209,1) 100%)',
                 height: '100vh',
                 padding: '100px',
             }}
         >
-            <Container maxWidth="sm">
+            <Container
+                maxWidth="xs"
+                sx={{
+                    backgroundColor: WRAPPER_BG,
+                    borderRadius: '25px',
+                    padding: '1.5rem 2rem 1rem',
+                }}
+            >
                 <CalcScreen result={result} expression={expression} />
                 <Grid container rowSpacing={1} columnSpacing={1}>
                     <Grid item xs={3}>
                         <CalcClearBtn
-                            style={{ backgroundColor: BUTTON_BG }}
+                            style={{ backgroundColor: BG }}
                             label="C"
                             handleClear={clearExpression}
                         />
@@ -129,7 +202,13 @@ const CalcApp = () => {
                     {BUTTONS.map((b) => (
                         <Grid item xs={3} key={b}>
                             <CalcBtn
-                                style={{ backgroundColor: BUTTON_BG }}
+                                style={{
+                                    backgroundColor:
+                                        NUMBERS.includes(b) ||
+                                        DECIMAL_POINT.includes(b)
+                                            ? BUTTON_BG
+                                            : BG,
+                                }}
                                 label={b}
                                 addOperator={addOperator}
                             />
--- src/components/mini2/calculatorUtils.js
+++ src/components/mini2/calculatorUtils.js
@@ -1,7 +1,7 @@
 export const NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 export const MATH_OPERATORS = ['+', '-', '*', '/', '%']
-export const SEPARATOR = '.'
-export const SIGN_OPERATOR = '+-'
+export const DECIMAL_POINT = ['.', ',']
+export const SIGN_OPERATORS = ['+-', '+/-']
 export const HIGH_OPERATORS = ['+', '-']
 export const LOW_OPERATORS = ['*', '/', '%']
 
