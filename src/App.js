4d312100d68c1a76d92347b3b71fbe00305a4a1c
leanhduy <laduy.hcmus@gmail.com>
2024-06-24 10:20:21 +0700

    Add the starter file for the branch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import { Header } from './components'
+
+function App() {
+    return (
+        <>
+            <Header title="Testing Header" />
+        </>
+    )
+}
+
+export default App
