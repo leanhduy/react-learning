a7640dabb7351fe6e7dd8a82aa299206393f8196
leanhduy <laduy.hcmus@gmail.com>
2024-07-01 19:21:59 +0700

    Add updates in sandbox

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
-import { GlobalStyles } from './styles'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './components/App'
+// import { GlobalStyles } from './styles'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+// const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(
-    <React.StrictMode>
-        <GlobalStyles />
-        <App />
-    </React.StrictMode>
-)
+// root.render(
+//     <React.StrictMode>
+//         <GlobalStyles />
+//         <App />
+//     </React.StrictMode>
+// )
+
+var myName = 'Duy'
+
+function greet(msg) {
+    console.log(msg, myName)
+}
+
+greet('hello')
